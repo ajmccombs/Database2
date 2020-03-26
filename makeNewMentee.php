625003c1bca5037e47a0b1b91b98805aa8166a93
Woom <theroyalblah@gmail.com>
2020-03-26 08:10:29 -0400

    fix mentee grade search

--- makeNewMentee.php
+++ makeNewMentee.php
@@ -22,7 +22,7 @@ $date = date('Ymd');
 
 $sql = "SELECT * FROM meetings WHERE group_id IN
 (SELECT group_id FROM groups
-    WHERE mentee_grade_req <= $grade)
+    WHERE mentee_grade_req = $grade)
     AND date NOT IN (
         SELECT date FROM meetings
         WHERE meet_id in (
