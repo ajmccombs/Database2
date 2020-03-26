6995c5b8a83c361da9af246f07fb5d1708e1fe49
Woom <theroyalblah@gmail.com>
2020-03-26 03:35:06 -0400

    view all meetings as admin

---
+++ viewMeetingsAdmin.php
@@ -0,0 +1,107 @@
+<a href='loginSuccess.php'>Return to Homepage</a>
+<br/>
+<?php
+
+    session_start();
+    define("BREAKLINE", "<br></br>");
+    $mysqli = mysqli_connect("localhost", "root", "", "db2");
+
+    // User needs to be an admin for this page
+    if($_SESSION["user"]["accountType"] != "admin") {
+        header("Location: homepage.php");
+
+        exit;
+    }
+
+    $id = $_SESSION["user"]["id"];
+    $sql = "SELECT * FROM meetings ORDER BY date ASC";
+
+    $result = $mysqli->query($sql);
+    if ($result->num_rows != 0) {
+        echo "All Meetings";
+    } else {
+        echo "There are no meetings";
+    }
+    echo BREAKLINE;
+?>
+
+<!-- Display table only if there are meetings to populate it.-->
+<?php if($result->num_rows != 0): ?>
+<table border="1">
+        <tr>
+            <th>Meeting Name</th>
+            <th>Required Mentor Level</th>
+            <th>Date</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+        </tr>
+        <?php
+            while($row = $result->fetch_assoc()) {
+                $time_id = $row["time_slot_id"];
+                $time_sql = "SELECT start_time, end_time FROM time_slot WHERE time_slot_id =  $time_id";
+                $time_result = $mysqli->query($time_sql);
+                $time_row = $time_result->fetch_assoc();
+
+                $grade_id = $row["group_id"];
+                $grade_sql = "SELECT mentor_grade_req FROM groups WHERE group_id = $grade_id";
+                $grade_result = $mysqli->query($grade_sql);
+                $grade_row = $grade_result->fetch_assoc();
+    
+                $gradelvl = $grade_row["mentor_grade_req"];
+                echo "<tr>";
+                echo "<td> " . $row["meet_name"] . "</td>";
+                echo "<td> " . $gradelvl . "</td>";
+                echo "<td> " . $row["date"] . "</td>";
+                echo "<td> " . $time_row["start_time"] . "</td>";
+                echo "<td> " . $time_row["end_time"] . "</td>";
+                echo "</tr>";
+
+            }
+        ?>
+
+</table>
+<?php endif; ?>
+<?php
+
+    echo BREAKLINE;
+    $id = $_SESSION["user"]["id"];
+    $sql = "SELECT * FROM meetings WHERE meet_id IN (SELECT meet_id FROM enroll WHERE mentee_id = $id)";
+
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows != 0) {
+        echo "Meetings where you are a mentee";
+    } else {
+        echo "You are not a mentee in any meetings.";
+    }
+    echo BREAKLINE;
+?>
+<?php if($result->num_rows != 0): ?>
+<table border="1">
+        <tr>
+            <th>Meeting Name</th>
+            <th>Grade Level</th>
+            <th>Date</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+        </tr>
+        <?php
+            while($row = $result->fetch_assoc()) {
+                $time_id = $row["time_slot_id"];
+                $time_sql = "SELECT start_time, end_time FROM time_slot WHERE time_slot_id =  $time_id";
+                $time_result = $mysqli->query($time_sql);
+                $time_row = $time_result->fetch_assoc();
+
+                echo "<tr>";
+                echo "<td> " . $row["meet_name"] . "</td>";
+                echo "<td> " . $row["group_id"] . "</td>";
+                echo "<td> " . $row["date"] . "</td>";
+                echo "<td> " . $time_row["start_time"] . "</td>";
+                echo "<td> " . $time_row["end_time"] . "</td>";
+                echo "</tr>";
+
+            }
+        ?>
+
+</table>
+<?php endif; ?>
